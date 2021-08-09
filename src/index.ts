2e013fb8462692bc61cee02ac8a63a4b1eef1de5
Trevor Manz <trevor.j.manz@gmail.com>
2021-08-09 15:15:19 -0400

    Upgrade gosling.js

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import { embed } from 'gosling.js/dist/src';
-import type { GoslingSpec } from 'gosling.js/dist/src';
+import { embed } from 'gosling.js';
+import type { GoslingSpec } from 'gosling.js';
 
-//@ts-ignore
+// @ts-ignore
 import css from 'higlass/dist/hglib.css';
+declare const css: string;
+
 const style = document.createElement('style');
 style.innerText = css;
 document.head.appendChild(style).setAttribute('type', 'text/css');
 
 type EmbedOptions = Parameters<typeof embed>[2];
 
-export function render(selector: string, spec: GoslingSpec, options: EmbedOptions = {}) {
+export function render(selector: string, spec: GoslingSpec, opts?: EmbedOptions) {
 	const el = document.getElementById(selector.substring(1))!;
-	embed(el, spec, options);
+	embed(el, spec, opts);
 }
 
 export { embed as goslingEmbed };
