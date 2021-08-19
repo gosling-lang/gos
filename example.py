3dcce9ed1c6256a00e319f97512c0e2bb7b9a312
Trevor Manz <trevor.j.manz@gmail.com>
2021-08-19 13:23:24 -0400

    feat: create composition API, deprecate gos.Chart for gos.View

--- example.py
+++ example.py
@@ -17,46 +17,47 @@ tooltip = [
     {"field": "sample", "type": "nominal", "alt": "Sample"},
 ]
 
-track1 = base.mark_rect().encode(
+view1 = base.mark_rect().encode(
     x=gos.Channel("start:G", axis="top"),
     xe="end:G",
     row=gos.Channel("sample:N", legend=True),
     color=gos.Channel("peak:Q", legend=True),
     tooltip=tooltip,
-)
+).view()
 
-track2 = base.mark_bar().encode(
+
+view2 = base.mark_bar().encode(
     x=gos.Channel("position:G", axis="top"),
     y="peak:Q",
     row="sample:N",
     color=gos.Channel("sample:N", legend=True),
     tooltip=tooltip,
-)
+).view()
 
-track3 = base.mark_bar().encode(
+
+view3 = base.mark_bar().encode(
     x=gos.Channel("position:G", axis="top"),
     y=gos.Channel("peak:Q", grid=True),
     color=gos.Channel("smaple:N", legend=True),
     tooltip=tooltip,
-)
+).view()
 
-track4 = base.encode(
+
+
+overlay_base = base.encode(
     x=gos.Channel("position:G", axis="top"),
     y="peak:Q",
     row="sample:N",
     color=gos.Channel("sample:N", legend=True),
     tooltip=tooltip,
-).properties(
-    alignment="overlay",
-    tracks=[
-        gos.PartialTrack().mark_line(),
-        gos.PartialTrack().mark_point().encode(
-            size=gos.Channel("peak:Q", range=[0, 2]),
-        )
-    ],
 )
 
-track5 = base.mark_point().encode(
+view4 = gos.overlay(
+    overlay_base.mark_line(),
+    overlay_base.mark_point().encode(size=gos.Channel("peak:Q", range=[0, 2])),
+)
+
+view5 = base.mark_point().encode(
     x=gos.Channel("position:G", axis="top"),
     y="peak:Q",
     row="sample:N",
@@ -64,18 +65,20 @@ track5 = base.mark_point().encode(
     color=gos.Channel("sample:N", legend=True),
     opacity={"value": 0.5},
     tooltip=tooltip,
-)
+).view()
 
-track6 = base.mark_point().encode(
+
+view6 = base.mark_point().encode(
     x=gos.Channel("position:G", axis="top"),
     y=gos.Channel("peak:Q", grid=True),
     size="peak:Q",
     color=gos.Channel("sample:N", legend=True),
     opacity={"value": 0.5},
     tooltip=tooltip,
-)
+).view()
 
-track7 = base.mark_area().encode(
+
+view7 = base.mark_area().encode(
     x=gos.Channel("position:G", axis="top"),
     y="peak:Q",
     row="sample:N",
@@ -83,9 +86,10 @@ track7 = base.mark_area().encode(
     stroke={"value": "white"},
     strokeWidth={"value": 0.5},
     tooltip=tooltip,
-)
+).view()
 
-track8 = base.mark_withinLink().encode(
+
+view8 = base.mark_withinLink().encode(
     x=gos.Channel(
         "s1:G", domain=gos.Domain(chromosome="1", interval=[103900000, 104100000])
     ),
@@ -96,28 +100,14 @@ track8 = base.mark_withinLink().encode(
     stroke={"value": "black"},
     strokeWidth={"value": 0.5},
     opacity={"value": 0.2},
-)
+).view()
 
-spec = gos.Chart(
+spec = gos.vertical(view1, view2, view3, view4, view5, view6, view7, view8).properties(
     title="Visual Encoding",
     subtitle="Gosling provides diverse visual encoding methods",
     layout="linear",
-    arrangement="vertical",
     centerRadius=0.8,
     xDomain=gos.Domain(chromosome="1", interval=[1, 3000500]),
-    views=[
-        {"tracks": [track]}
-        for track in [
-            track1,
-            track2,
-            track3,
-            track4,
-            track5,
-            track6,
-            track7,
-            track8,
-        ]
-    ],
 )
 
 print(spec.to_dict())
--- gosling/api.py
+++ gosling/api.py
@@ -38,7 +38,7 @@ class _PropertiesMixen:
         return copy
 
 
-class Chart(_PropertiesMixen, core.Root):
+class View(_PropertiesMixen, core.Root):
     def _repr_mimebundle_(self, include=None, exclude=None):
         dct = self.to_dict()
         renderer = renderers.get("js")
@@ -50,6 +50,52 @@ class Chart(_PropertiesMixen, core.Root):
         display(self)
 
 
+def horizontal(*views, **kwargs):
+    return View(arrangement="horizontal", views=views, **kwargs)
+
+
+def vertical(*views, **kwargs):
+    return View(arrangement="vertical", views=views, **kwargs)
+
+
+def parallel(*views, **kwargs):
+    return View(arrangement="parallel", views=views, **kwargs)
+
+
+def serial(*views, **kwargs):
+    return View(arrangement="serial", views=views, **kwargs)
+
+
+# View utilities
+
+
+def overlay(*tracks, **kwargs):
+    """Compose an overlaid view from multiple tracks or overliad tracks."""
+    # Overlay requires a `width` and `height`. Check if provided as kwargs, otherwise
+    # eagerly grab the width/height from the tracks, otherwise use defaults.
+    width = kwargs.pop(
+        "width", next((t.width for t in tracks if t.width != Undefined), DEFAULT_WIDTH)
+    )
+
+    height = kwargs.pop(
+        "height",
+        next((t.height for t in tracks if t.height != Undefined), DEFAULT_HEIGHT),
+    )
+
+    # TODO: Gosling.js doesn't respect the parent width/height if defined in children.
+    # This is a hack to ensure the children respect the view-level config.
+    tracks = [t.properties(width=Undefined, height=Undefined) for t in tracks]
+
+    return View(
+        alignment="overlay", tracks=tracks, width=width, height=height, **kwargs
+    )
+
+
+def stack(*tracks, **kwargs):
+    """Compose a stacked view from multiple tracks or overlaid tracks."""
+    return View(alignment="stack", tracks=tracks, **kwargs)
+
+
 class Track(_EncodingMixin, _PropertiesMixen, mixins.MarkMethodMixin, core.Track):
     def __init__(self, data=Undefined, **kwargs):
         super().__init__(
@@ -59,11 +105,5 @@ class Track(_EncodingMixin, _PropertiesMixen, mixins.MarkMethodMixin, core.Track
             **kwargs,
         )
 
-    def chart(self, **kwargs):
-        return Chart(tracks=[self.copy()], **kwargs)
-
-
-class PartialTrack(
-    _EncodingMixin, _PropertiesMixen, mixins.MarkMethodMixin, core.PartialTrack
-):
-    ...
+    def view(self, **kwargs):
+        return View(tracks=[self], **kwargs)
