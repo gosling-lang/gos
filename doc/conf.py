34c82efc54c33f6a1f88b1423837dbf9543d9c8f
Trevor Manz <trevor.j.manz@gmail.com>
2021-09-06 23:19:30 -0400

    doc: add favicon and styling

--- doc/conf.py
+++ doc/conf.py
@@ -1,18 +1,10 @@
 # !/usr/bin/env python3
-#
+
 import sys
 import os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
 
-# -- General configuration ------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
@@ -28,65 +20,36 @@ autodoc_default_flags = ["members", "inherited-members"]
 
 autodoc_member_order = "groupwise"
 
-# generate autosummary even if no references
 autosummary_generate = True
 
 numpydoc_show_class_members = False
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = ".rst" # ['.rst', '.md']
 
-# The master toctree document.
 master_doc = "index"
 
-# General information about the project.
 project = "gos"
 copyright = "2021, Trevor Manz"
 author = "Trevor Manz"
 
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
 version = "0.1.0"
-# The full version, including alpha/beta/rc tags.
 release = version
 
-# This is also used if you do content translation via gettext catalogs.
-# Usually you set "language" from the command line for these cases.
-language = None
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-# -- Options for HTML output ----------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 html_theme = 'furo'
 
-# A shorter title for the navigation bar.  Default is the same as html_title.
+html_title = "gos"
 html_short_title = "gos"
 
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = "_static/altair-logo-light.png"
+# The name of an image file (relative to this directory) to place at the top of the sidebar.
+html_logo = "_static/favicon.png"
 
-# The name of an image file (relative to this directory) to use as a favicon of
-# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = "_static/favicon.ico"
+html_favicon = "_static/favicon.ico"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static", "_images"]
+html_static_path = ["_static"]
+
+html_css_files = ["theme_overrides.css"]
+
--- doc/index.rst
+++ doc/index.rst
@@ -1,10 +1,10 @@
-**gos**: Declarative (epi)genomic visualization in Python
-=========================================================
+**gos**: (epi)genomic visualization in python
+=============================================
 .. role:: raw-html(raw)
    :format: html
 
 **gos** is a declarative (epi)genomics visualization library for Python. It is built on 
-top of the _Gosling JSON specification, providing an simplified interface for authoring 
+top of the `Gosling`_ JSON specification, providing an simplified interface for authoring 
 interactive genomic visualizations. The source is available on
 `GitHub <http://github.com/manzt/gos>`_.
 
