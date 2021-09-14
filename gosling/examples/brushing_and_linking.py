0bdff7ee433ff32a63729e2c0b8fc8946d43d240
Trevor Manz <trevor.j.manz@gmail.com>
2021-09-14 09:05:54 -0400

    refactor: break up example into variables

--- gosling/examples/brushing_and_linking.py
+++ gosling/examples/brushing_and_linking.py
@@ -6,7 +6,7 @@ Brushing and Linking
 import gosling as gos
 from gosling.data import multivec
 
-mv = multivec(
+data = multivec(
     url="https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
     row="sample",
     column="position",
@@ -14,29 +14,30 @@ mv = multivec(
     categories=["sample 1"],
 )
 
-base = gos.Track(mv).encode(y="peak:Q").properties(width=800, height=200)
+base = gos.Track(data).encode(y="peak:Q").properties(width=800, height=200)
 
-gos.stack(
-    gos.overlay(
-        base.mark_line().encode(
-            x=gos.Channel("position:G", domain=gos.Domain(chromosome="1"), axis="top")
-        ),
-        base.mark_brush().encode(
-            x=gos.Channel(
-                "position:G",
-                domain=gos.Domain(chromosome="1"),
-                axis="top",
-                linkingId="linking-with-brush"
-            ),
-            color=gos.value("steelBlue")
-        ),
+top =  gos.overlay(
+    base.mark_line().encode(
+        x=gos.Channel("position:G", domain=gos.Domain(chromosome="1"), axis="top")
     ),
-    base.mark_line(background="steelBlue", backgroundOpacity=0.1).encode(
+    base.mark_brush().encode(
         x=gos.Channel(
             "position:G",
-            domain=gos.Domain(chromosome="1", interval=[200000000, 220000000]),
+            domain=gos.Domain(chromosome="1"),
             axis="top",
-            linkingId="linking-with-brush",
-        )
+            linkingId="linking-with-brush"
+        ),
+        color=gos.value("steelBlue")
+    ),
+)
+
+bottom = base.mark_line(background="steelBlue", backgroundOpacity=0.1).encode(
+    x=gos.Channel(
+        "position:G",
+        domain=gos.Domain(chromosome="1", interval=[200000000, 220000000]),
+        axis="top",
+        linkingId="linking-with-brush",
     )
 )
+
+gos.stack(top, bottom)
