e3a65af2e7d056b8911283336497a5add2ec9f2a
Trevor Manz <trevor.j.manz@gmail.com>
2021-09-17 12:34:05 -0400

    v0.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ data_server_requirements = [
 
 setuptools.setup(
     name="gosling",
-    version="0.0.4",
+    version="0.0.5",
     author="Trevor Manz",
     author_email="trevor.j.manz@gmail.com",
     description="Python bindings to generate Gosling schema.",
