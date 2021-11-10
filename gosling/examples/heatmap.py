1912bcf07e1330e579321366ff214eb4fe0e8bde
Trevor Manz <trevor.j.manz@gmail.com>
2021-11-10 13:24:35 -0500

    doc: add simple Hi-C heatmap (#83)

---
+++ gosling/examples/heatmap.py
@@ -0,0 +1,18 @@
+"""
+Hi-C Heatmap
+==============
+"""
+# category: basic marks
+import gosling as gos
+ 
+size = 500
+data = gos.matrix("https://server.gosling-lang.org/api/v1/tileset_info/?d=hffc6-microc-hg38")
+# data = gos.matrix('/path/to/dataset.cool') # local dataset
+    
+track = gos.Track(data).mark_rect().encode(
+  x="position1:G",
+  y="position2:G",
+  color=gos.Channel("value:Q", range="warm"),
+).properties(width=size, height=size)
+
+track.view()
