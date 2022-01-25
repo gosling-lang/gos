f1569f5154790150686ccfd6e5caac9e006d32e6
Trevor Manz <trevor.j.manz@gmail.com>
2022-01-25 10:12:24 -0500

    Update gosling/examples/heatmap.py
    Co-authored-by: Sehi L'Yi <sehi_lyi@hms.harvard.edu>

--- gosling/examples/heatmap.py
+++ gosling/examples/heatmap.py
@@ -10,9 +10,9 @@ data = gos.matrix("https://server.gosling-lang.org/api/v1/tileset_info/?d=hffc6-
 # data = gos.matrix('/path/to/dataset.cool') # local dataset
     
 track = gos.Track(data).mark_bar().encode(
-  x="xs:G",
+  x=gos.X("xs:G", axis="bottom"),
   xe="xe:G",
-  y="ys:G",
+  y=gos.Y("ys:G", axis="left"),
   ye="ye:G",
   color=gos.Color("value:Q", range="warm", legend=True),
 ).properties(width=size, height=size)
