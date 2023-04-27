d3b574d210de8cd91014fd7f887bf4b1302300ea
Trevor Manz <trevor.j.manz@gmail.com>
2023-04-27 12:51:17 -0400

    feat: upgrade gosling.js to v0.9.29 (#130)

--- gosling/schema/__init__.py
+++ gosling/schema/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from .core import *
 from .channels import *
-SCHEMA_VERSION = 'v0.9.28'
-SCHEMA_URL = 'https://raw.githubusercontent.com/gosling-lang/gosling.js/v0.9.28/schema/gosling.schema.json'
+SCHEMA_VERSION = 'v0.9.29'
+SCHEMA_URL = 'https://raw.githubusercontent.com/gosling-lang/gosling.js/v0.9.29/schema/gosling.schema.json'
 THEMES = {'dark', 'ensembl', 'excel', 'ggplot', 'google', 'igv', 'jbrowse', 'light', 'ucsc', 'warm', 'washu'}
