a94b22145cda1c11a05260bfcde2ab136f8b11db
sehilyi <sehilyi@users.noreply.github.com>
2023-12-14 16:09:21 +0000

    Bump Gosling.js to `v0.13.1`

--- gosling/schema/__init__.py
+++ gosling/schema/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from .core import *
 from .channels import *
-SCHEMA_VERSION = 'v0.9.31'
-SCHEMA_URL = 'https://raw.githubusercontent.com/gosling-lang/gosling.js/v0.9.31/schema/gosling.schema.json'
+SCHEMA_VERSION = 'v0.13.1'
+SCHEMA_URL = 'https://raw.githubusercontent.com/gosling-lang/gosling.js/v0.13.1/src/gosling-schema/gosling.schema.json'
 THEMES = {'dark', 'ensembl', 'excel', 'ggplot', 'google', 'igv', 'jbrowse', 'light', 'ucsc', 'warm', 'washu'}
