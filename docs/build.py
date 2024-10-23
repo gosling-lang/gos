d3d192d19233bc481f6b611e801f7d65a6a9eb3d
Trevor Manz <trevor.j.manz@gmail.com>
2024-10-22 23:34:19 -0400

    Move docs

---
+++ docs/build.py
@@ -0,0 +1,33 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "furo",
+#     "gosling",
+#     "numpydoc>=1.5",
+#     "sphinx>=6.0",
+# ]
+#
+# [tool.uv.sources]
+# gosling = { path = "../" }
+# ///
+import pathlib
+
+from sphinx.application import Sphinx
+
+SELF_DIR = pathlib.Path(__file__).parent
+
+
+def main():
+    app = Sphinx(
+        srcdir=SELF_DIR,
+        confdir=SELF_DIR,
+        outdir=SELF_DIR / "dist",
+        doctreedir=SELF_DIR / ".doctrees",
+        buildername="html",
+    )
+
+    app.build()
+
+
+if __name__ == "__main__":
+    main()
